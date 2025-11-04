cd06b1030d83e7e847694f874081041fc4eecb45
Mariana Nikaido <mariananikaido04@gmail.com>
2025-11-03 21:45:27 -0300

    Adicionando backend e frontend completos

--- app/services/api.js
+++
@@ -1,9 +0,0 @@
-// app/services/api.js
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'http://192.168.15.7:3000', // 👈 seu IP local e porta do backend
-  timeout: 10000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
